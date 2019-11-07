4f484eceea8f3546e501d2875629936ff8f57475
Jeff Boenig <jboenig@nc.rr.com>
2019-11-07 09:42:47 -0500

    Add copyright to all source files

--- WorkflowViewModes.cs
+++ WorkflowViewModes.cs
@@ -1,4 +1,23 @@
-﻿namespace Headway.WorkflowEngine
+﻿////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019 Jeff Boenig
+//
+// This file is part of Headway.WorkflowEngine.
+//
+// Headway.WorkflowEngine is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// Headway.WorkflowEngine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with Headway.WorkflowEngine. If not, see http://www.gnu.org/licenses/.
+////////////////////////////////////////////////////////////////////////////////
+
+namespace Headway.WorkflowEngine
 {
     /// <summary>
     /// Defines different modes for viewing workflow subjects.
