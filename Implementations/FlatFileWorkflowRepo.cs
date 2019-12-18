727703d85b1582710bc39a8e4afb0756a8e1ac74
Jeff Boenig <jboenig@nc.rr.com>
2019-12-18 14:59:59 -0500

    Add FlatFileRepo implementation for Workflow objects

---
+++ Implementations/FlatFileWorkflowRepo.cs
@@ -0,0 +1,61 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019 Jeff Boenig
+//
+// This file is part of Headway.WorkflowEngine.
+//
+// Headway.WorkflowEngine is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// Headway.WorkflowEngine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with Headway.WorkflowEngine. If not, see http://www.gnu.org/licenses/.
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Linq;
+using Headway.Dynamo.Serialization;
+using Headway.Dynamo.Repository.FlatFileRepo;
+using Headway.WorkflowEngine.Resolvers;
+
+namespace Headway.WorkflowEngine.Implementations
+{
+    /// <summary>
+    /// Implements a <see cref="FlatFileRepo{TObject}"/> to
+    /// store and retrieve <see cref="Workflow"/> objects.
+    /// </summary>
+    public sealed class FlatFileWorkflowRepo : FlatFileRepo<Workflow>,
+        IWorkflowByNameResolver
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="serializerConfigSvc"></param>
+        /// <param name="svcProvider"></param>
+        public FlatFileWorkflowRepo(string filePath,
+              ISerializerConfigService serializerConfigSvc,
+              IServiceProvider svcProvider) :
+            base(filePath, serializerConfigSvc, svcProvider)
+        {
+        }
+
+        /// <summary>
+        /// Resolves a <see cref="Workflow"/> object given a
+        /// fully-qualified workflow name.
+        /// </summary>
+        /// <param name="workflowName">Fully qualified name of the workflow</param>
+        /// <returns>Workflow matching the given workflow name.</returns>
+        public Workflow Resolve(string workflowName)
+        {
+            return (from w in this.GetQueryable()
+                    where w.FullName == workflowName
+                    select w).FirstOrDefault();
+        }
+    }
+}
---
+++ UnitTests/WorkflowFlatFileRepoTests.cs
@@ -0,0 +1,71 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019 Jeff Boenig
+//
+// This file is part of Headway.WorkflowEngine.
+//
+// Headway.WorkflowEngine is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// Headway.WorkflowEngine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with Headway.WorkflowEngine. If not, see http://www.gnu.org/licenses/.
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+
+using Headway.Dynamo.Metadata;
+using Headway.Dynamo.Serialization;
+using Headway.Dynamo.Exceptions;
+using Headway.WorkflowEngine.UnitTests.MockData;
+using Headway.WorkflowEngine;
+using Headway.WorkflowEngine.Resolvers;
+using Headway.WorkflowEngine.Implementations;
+using Headway.Dynamo.Repository;
+using Headway.Dynamo.Repository.FlatFileRepo;
+
+namespace WorkflowEngine.UnitTests
+{
+    [TestClass]
+    public class WorkflowFlatFileRepoTests
+    {
+        private IKernel kernel;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.kernel = new StandardKernel();
+            this.kernel.Bind<IServiceProvider>().ToConstant(this.kernel);
+            this.kernel.Bind<IWorkflowByNameResolver>().To<FlatFileWorkflowRepo>()
+                .WithConstructorArgument("filePath", "workflows.json");
+            this.kernel.Bind<IObjectRepository<Workflow>>().To<FlatFileWorkflowRepo>()
+                .WithConstructorArgument("filePath", "workflows.json");
+            this.kernel.Bind<ISerializerConfigService>().To<StandardSerializerConfigService>();
+            this.kernel.Bind<IMetadataProvider>().To<StandardMetadataProvider>();
+        }
+
+        [TestMethod]
+        public void CreateAndSaveWorkflowTest1()
+        {
+            var workflow = new Workflow("Test.Foo");
+            workflow.InitialState = new WorkflowState("Reviewing");
+            workflow.States.Add(new WorkflowState("Complete"));
+            workflow.InitialState.Transitions.Add(new WorkflowTransition("Approve", "Complete"));
+
+            var workflowRepo = this.kernel.GetService(typeof(IObjectRepository<Workflow>)) as IObjectRepository<Workflow>;
+            workflowRepo.Add(workflow);
+            workflowRepo.SaveChanges();
+            var workflowByNameResolver = this.kernel.GetService(typeof(IWorkflowByNameResolver)) as IWorkflowByNameResolver;
+
+            var resWorkflow = workflowByNameResolver.Resolve("Test.Foo");
+            Assert.IsNotNull(resWorkflow);
+        }
+    }
+}
